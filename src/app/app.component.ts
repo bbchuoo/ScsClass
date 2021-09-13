2487657ba822540c4d1e7112ea99656301baaeb3
alvinhsu <alvinhsu>
2021-09-13 13:27:49 +0800

    add

---
+++ src/app/app.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  str
+  str1;
+  fileToUpload: File = null;
+  file: any;
+  bb = [];
+  cc = [];
+  dd = []
+  ee = []
+  yyy = [];
+  res;
+  title = 'alvinTest';
+  showC = false;
+
+  constnt = [];
+  click() {
+    this.showC = !this.showC;
+  }
+
+  ngOnInit(): void {
+
+    // this.test()
+  }
+
+  mergeRes(res?) {
+    this.res = res;
+    this.bb = [];
+    this.cc = [];
+    this.dd = [];
+    this.ee = [];
+    this.yyy = [];
+
+    console.log('this.str =', this.str);
+
+    var arr = this.str.split('\n');
+
+    console.log('this.str =', this.str);
+
+    console.log('arr=', arr)
+
+    const newArr = [];
+    arr.forEach(element => {
+
+      const str = element.replace(/\s+/g, ' ');
+      const aa = str.split(" ");
+      if (aa.length === 3) {
+        this.bb.push(aa[0]);
+        this.cc.push(aa[2]);
+      }
+      if (aa.length === 4) {
+        this.bb.push(aa[0] + '轉物件');
+        this.cc.push(aa[3] + '轉物件');
+      }
+      console.log('aa=', aa)
+    });
+
+    console.log('this.bb=', this.bb)
+    console.log('this.cc=', this.cc)
+
+    this.bb.forEach((st: string) => {
+      if (st.indexOf('轉物件') > -1 && res) {
+        const stt = st.replace('轉物件', '');
+        this.dd.push(stt + `:${stt[0].toUpperCase() + stt.slice(1)};<br/>`);
+      } else if (st.indexOf('轉物件') > -1 && !res) {
+        const stt = st.replace('轉物件', '');
+        this.dd.push(stt + ` = new ${stt[0].toUpperCase() + stt.slice(1)}();<br/>`);
+      } else if (res) {
+        this.dd.push(st + ':string;<br/>');
+      } else {
+        this.dd.push(st + ' = \'\';<br/>');
+      }
+    });
+
+
+    this.cc.forEach((st) => {
+      this.ee.push('/** ' + st + ' */' + '<br/>')
+    });
+
+
+    console.log('this.dd=', this.dd)
+    console.log('this.ee=', this.ee)
+
+
+    this.ee.forEach((eee, index) => {
+      this.yyy.push(eee);
+      this.yyy.push(this.dd[index]);
+    });
+
+    console.log('this.yyy=', this.yyy)
+
+  }
+
+}
