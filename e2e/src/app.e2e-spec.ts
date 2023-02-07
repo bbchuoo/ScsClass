052c7f2eba8cfd6de5bb10eaacffcd0e7d9d57fb
Alvin <bbchuoo0523@gmail.com>
2023-02-07 17:12:23 +0800

    修改專案名稱

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('scsClass app is running!');
+    expect(page.getTitleText()).toEqual('ScsClass app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'scsClass'`, () => {
+  it(`should have as title 'ScsClass'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('scsClass');
+    expect(app.title).toEqual('ScsClass');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('scsClass app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ScsClass app is running!');
   });
 });
