3fac8d1a51b288041649faa99ce73037fb2ecd8d
Alvin <bbchuoo0523@gmail.com>
2023-02-07 17:04:03 +0800

    更新專案名稱

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'scsbCorpClass'`, () => {
+  it(`should have as title 'scsClass'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('scsbCorpClass');
+    expect(app.title).toEqual('scsClass');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('scsbCorpClass app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('scsClass app is running!');
   });
 });
